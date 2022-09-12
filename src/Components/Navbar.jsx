250744a4ab64101f3971df5379699c5cf4329ba8
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-09-12 17:19:59 +0530

    mart

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <div className="nav-div">
+      <ul className="u-list">
+        <li>
+          <a href="#">home</a>
+        </li>
+        <li>
+          <a href="#">product</a>
+        </li>
+        <li>
+          <a href="#">search</a>
+
+        </li>
+        <li>
+          <a href="#">help</a>
+          
+        </li>
+        <li>
+          <a href="#">contact</a>
+          
+        </li>
+      </ul>
+      
+
+      
+    </div>
+  );
+};
+
+export default Navbar;
